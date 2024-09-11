0a31280b7523b0d811e0ae6d9eaf4ad9ab43e4c1
dylee00 <dylee6820@gmail.com>
2024-09-11 14:24:37 +0900

    코드 수정

--- week02/helloE.c
+++ week02/helloE.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 
 int main(int argc, char *argv[]){
-    printf("Hello, Sookmyung Electronics!\n");
+    int a, b; //variables
 
-    return 0;
+    a = 10;
+    b = 20;
+
+    printf("Hello, Sookmyung Electronics!\n");
+    printf("%d + %d = %d\n  ",  a, b, a+b);
+    return 0;  
 }
